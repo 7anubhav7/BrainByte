b5877d32f82b3f2404aaf83746a90db3cb88e2b7
anubhav <anubhavhajela37@gmail.com>
2025-06-26 19:28:35 +0530

    Implemented footer component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 "use client";
 import styles from "./page.module.css";
-
+import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import Tabs from "@/components/tab/Tab";
 
@@ -10,6 +10,7 @@ export default function Home() {
     <div>
       <Navbar />
       <Tabs />
+      <Footer />
     </div>
   );
 }
